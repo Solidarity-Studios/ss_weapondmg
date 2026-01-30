fa379455d4fba7b7c05fe13ec3200259b46fdeb8
DivineChaos | Solidarity <64466246+mariahantionette@users.noreply.github.com>
2026-01-30 03:28:58 -0600

    Update description and format client scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Solidarity Studios | buildsolidarity@gmail.com'
-description 'Weapon Damage Multiplier'
+description 'Damage Multiplier'
 version '1.0.0'
 
-client_script 'cl_main.lua'
+client_scripts {
+  'cl_main.lua'
+}
